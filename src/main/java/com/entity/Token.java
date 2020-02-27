021de5b6348a58bcef19d07461fbf5e8ddda88cb
zhu_kui <2551781052@qq.com>
2020-02-27 11:52:46 +0800

    zk

--- src/main/java/com/entity/Token.java
+++ src/main/java/com/entity/Token.java
@@ -1,6 +1,69 @@
 public class Token{
 
-    public static void main(String[] args) {
+    private int id;
+    private String token;
+    private String openid;
+    private String session_key;
+    private timestamp createtime;
+    private int status;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getSession_key() {
+        return session_key;
+    }
+
+    public void setSession_key(String session_key) {
+        this.session_key = session_key;
+    }
+
+    public timestamp getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(timestamp createtime) {
+        this.createtime = createtime;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Token{" +
+                "id=" + id +
+                ", token='" + token + '\'' +
+                ", openid='" + openid + '\'' +
+                ", session_key='" + session_key + '\'' +
+                ", createtime=" + createtime +
+                ", status=" + status +
+                '}';
     }
 }
